b433290098654f6e938b846fdf6e3cd0b3087217
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-03-07 21:32:56 +0100

    Final

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,3 +1,18 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// part 'item_model.freezed.dart';
+
+// @freezed
+// class ItemModel with _$ItemModel {
+//   factory ItemModel(
+//     String name,
+//     String info,
+//     String flutter,
+//     String snowboard,
+//   ) = _ItemModel;
+//   factory ItemModel.fromJson(Map<String, dynamic> json) =>
+//       _$ItemModelFromJson(json);
+// }
+
 class ItemModel {
   ItemModel(
       {required this.name,
