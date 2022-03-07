fd485ed57369bf94e8e689fb2a33c1c9beef5cff
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-03-07 16:51:52 +0100

    Add firebase read

--- lib/features/pulpit_page/pulpit.dart
+++ lib/features/pulpit_page/pulpit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,138 +25,171 @@ class Pulpit extends StatelessWidget {
                     onPressed: () async {
                       await FirebaseAuth.instance.signOut();
                     },
-                    icon: Icon(Icons.person))
+                    icon: const Icon(Icons.person))
               ],
               centerTitle: true,
               backgroundColor: Colors.purple,
-              title: Text('Flutter Academy Task Zero'),
+              title: const Text('Flutter Academy Task Zero'),
             ),
             backgroundColor: Colors.grey[400],
             body: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Center(
-                  child: StaggeredGrid.count(
-                crossAxisCount: 3,
-                mainAxisSpacing: 1,
-                crossAxisSpacing: 1,
-                children: [
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: state.inkColor,
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                      ),
-                      child: InkWell(
-                        onTap: () {
-                          context.read<PulpitCubit>().mixColors();
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Center(
-                              child: Text(
-                            "Łukasz",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          )),
-                        ),
-                      ),
+              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream:
+                    FirebaseFirestore.instance.collection('data').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("Coś poszło nie tak");
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: Text("Ładowanie danych"));
+                  }
+                  final documents = snapshot.data!.docs;
+
+                  return Center(
+                    child: StaggeredGrid.count(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 1,
+                      crossAxisSpacing: 1,
+                      children: [
+                        for (final document in documents) ...[
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 1,
+                            mainAxisCellCount: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: state.inkColor,
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                              ),
+                              child: InkWell(
+                                onTap: () {
+                                  context.read<PulpitCubit>().mixColors();
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Center(
+                                      child: Text(
+                                    document['name'],
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  )),
+                                ),
+                              ),
+                            ),
+                          ),
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 1,
+                            mainAxisCellCount: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: state.inkColor,
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                              ),
+                              padding: const EdgeInsets.all(8),
+                              child: InkWell(
+                                onTap: () {
+                                  context.read<PulpitCubit>().mixColors2();
+                                },
+                                child: Text(
+                                  document['info'],
+                                ),
+                              ),
+                            ),
+                          ),
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 1,
+                            mainAxisCellCount: 1,
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              child: InkWell(
+                                onTap: () {
+                                  context.read<PulpitCubit>().mixColors2();
+                                },
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                                image: const DecorationImage(
+                                  image: AssetImage("images/snowboard.jpg"),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 1,
+                            mainAxisCellCount: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: state.inkColor,
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                              ),
+                              padding: const EdgeInsets.all(8),
+                              child: InkWell(
+                                onTap: () {
+                                  context.read<PulpitCubit>().mixColors2();
+                                },
+                              ),
+                              // child: ElevatedButton(
+                              //   onPressed: () {},
+                              //   child: const Text('Naciśnij by podmienić'),
+                              // ),
+                            ),
+                          ),
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 2,
+                            mainAxisCellCount: 2,
+                            child: Container(
+                              child: InkWell(onTap: () {
+                                context.read<PulpitCubit>().mixColors();
+                              }),
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                                image: const DecorationImage(
+                                  image: const AssetImage("images/Foto.jpg"),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                          StaggeredGridTile.count(
+                            crossAxisCellCount: 1,
+                            mainAxisCellCount: 1,
+                            child: Container(
+                              child: InkWell(
+                                onTap: () {
+                                  context.read<PulpitCubit>().mixColors2();
+                                },
+                                child: Image.network(
+                                  'https://ih1.redbubble.net/image.1060739513.9977/pp,840x830-pad,1000x1000,f8f8f8.u1.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                color: state.inkColor,
+                                border: Border.all(
+                                    width: state.borderWidth,
+                                    color: state.borderColor),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ],
                     ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: state.inkColor,
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: InkWell(
-                          onTap: () {
-                            context.read<PulpitCubit>().mixColors2();
-                          },
-                          child: const Text(
-                              'Rozwinięcia swoich umiejętności i możliwości przyszłego zatrudnienia.')),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: InkWell(
-                        onTap: () {
-                          context.read<PulpitCubit>().mixColors2();
-                        },
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                        image: DecorationImage(
-                          image: AssetImage("images/snowboard.jpg"),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: state.inkColor,
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: InkWell(
-                        onTap: () {
-                          context.read<PulpitCubit>().mixColors2();
-                        },
-                      ),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 2,
-                    child: Container(
-                      child: InkWell(onTap: () {
-                        context.read<PulpitCubit>().mixColors();
-                      }),
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                        image: DecorationImage(
-                          image: AssetImage("images/Foto.jpg"),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: state.inkColor,
-                        border: Border.all(
-                            width: state.borderWidth, color: state.borderColor),
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: InkWell(
-                        onTap: () {
-                          context.read<PulpitCubit>().mixColors2();
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              )),
+                  );
+                },
+              ),
             ),
           );
         },
